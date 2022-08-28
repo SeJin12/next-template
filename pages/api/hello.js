a426bbfbc9d9bbc60b35bf37072efcfe0842a704
SeJin12 <tpwls8122@naver.com>
2022-08-28 22:29:21 +0900

    absolute path, Movie, basic reducer, css

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,15 +4,15 @@ import { defaultGet } from "@lib/http";
 
 /**
  * @example
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
 export default function handler(req, res) {
-  const url = "http://192.168.0.11:8080/v1/market/all";
-
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
+  console.log(req.method, req.query);
   defaultGet(url, {})
     .then((response) => {
-      res.status(200).json(response.data);
+      res.status(200).json(response.data.results);
     })
     .catch(({ err }) => {
       res.status(500).json({ err });
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,34 @@
 import useSWR, { mutate } from "swr";
 import { fetcherGet } from "@lib/http";
+import Moive from "@ui/Moive";
 
 export default function Home() {
+
+  // const [page, setPage] = useState(1);
   const { data, mutate, error } = useSWR(`/api/hello`, fetcherGet);
 
-  if (error) return <div>Error..!</div>;
+  if (error) return <div> {error.toString()} Error..!</div>;
   if (!data) return <div>Loading...</div>;
 
   return (
     <div>
-      <button onClick={() => mutate()}>호출</button>
+      <button onClick={() => mutate()}>호출(SWR mutate)</button>
       <ul>
-        {data.map((d) => (
-          <li key={d.market}>{d.korean_name}</li>
+        {data.filter((d, index) => index % 5 == 0).map((d) => (
+          <li key={d.id}>
+            <Moive title={d.title} overview={d.overview} poster_path={d.poster_path} release_date={d.release_date} />
+          </li>
         ))}
       </ul>
       <style jsx>
         {`
           ul {
             list-style: none;
+            column-count: 2;
+          }
+          li {
+            margin: 10px;
+            width: 400px;
           }
         `}
       </style>
