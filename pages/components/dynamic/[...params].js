d2a6aa985ac2052b9015a901c3c41773b4cf0482
SeJin12 <tpwls8122@naver.com>
2022-09-03 00:22:14 +0900

    Router query, masking URL

--- pages/components/dynamic/[...params].js
+++ pages/components/dynamic/[...params].js
@@ -2,14 +2,17 @@ import { useRouter } from "next/router";
 
 export default function Data({ params }) {
   const router = useRouter();
-  const [title, id] = params || [];
+  const [title] = params || [];
 
-  console.log("index: ", router);
+  // console.log("index: ", router);
+  // console.log("params: ", params);
+  // console.log("router.query: ", router.query);
+  // console.log(router.query.params);
   return (
     <div>
       title: {title}
       <hr />
-      id: {id}
+      id: {router.query.id}
     </div>
   );
 }
